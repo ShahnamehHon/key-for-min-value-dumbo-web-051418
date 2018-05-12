81aebdcd0707e6232d1b6cc380e13c7d5b3786de
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-12 21:40:30 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,10 @@
 def key_for_min_value(name_hash)
 key = nil
 value = nil
-name_hash.collect { |k, v|
+name_hash.collect do |k, v|
 if value == nil || v < value
   key = k
   value = v
-  }
 end
 key
 end
